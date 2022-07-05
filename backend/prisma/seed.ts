143a256c6a60f0e8cfbf83ff691873276a44f1f1
TakaoSone <takao.sone.0616@gmail.com>
2022-07-06 06:17:04 +0900

    backend: remove sample

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,24 +1,7 @@
 import { Logger } from '@nestjs/common';
-import { Prisma, PrismaClient, Role, RoleName, Sample, User } from '@prisma/client';
+import { Prisma, PrismaClient, Role, RoleName, User } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
-const sampleData: Sample[] = [
-  {
-    id: 1,
-    name: 'Foo',
-    displayedId: 'ABC-DEF-GHI',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 2,
-    name: 'Bar',
-    displayedId: '123-456-789',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
 
 const roleCreationData: Prisma.RoleCreateInput[] = [
   {
@@ -49,17 +32,6 @@ const userCreationData: Prisma.UserCreateInput[] = [
   },
 ];
 
-const doSeedSample = async (sampleData: Sample[]) => {
-  const samples: Prisma.Prisma__SampleClient<Sample>[] = [];
-  for (const sample of sampleData) {
-    const createPosts = prisma.sample.create({
-      data: sample,
-    });
-    samples.push(createPosts);
-  }
-  return await prisma.$transaction(samples);
-};
-
 const doSeedRoles = async (createInputs: Prisma.RoleCreateInput[]) => {
   const roles: Prisma.Prisma__RoleClient<Role>[] = [];
   for (const input of createInputs) {
@@ -85,7 +57,6 @@ const doSeedUsers = async (createInputs: Prisma.UserCreateInput[]) => {
 const main = async () => {
   Logger.log(`Start seeding ...`);
 
-  await doSeedSample(sampleData);
   await doSeedRoles(roleCreationData);
   await doSeedUsers(userCreationData);
 
--- backend/src/modules/sample/sample.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SampleResolver } from './sample.resolver';
-
-@Module({
-  providers: [SampleResolver],
-})
-export class SampleModule {}
