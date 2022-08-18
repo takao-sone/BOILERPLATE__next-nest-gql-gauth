417572d3beecbe7142116a0c327b85b62a6b9418
TakaoSone <takao.sone.0616@gmail.com>
2022-08-19 02:58:40 +0900

    .

---
+++ infrastructure/staging/modules/AppRunner/variables.tf
@@ -0,0 +1,13 @@
+# Base ========================
+variable "project_name" {
+  type = string
+}
+
+variable "project_stg" {
+  type = string
+}
+
+# Github ===========================
+
+
+# AppRunner ===========================
