24092416567fd55529268258f1e8050d706feb59
TakaoSone <takao.sone.0616@gmail.com>
2022-09-04 01:47:07 +0900

    .

--- infrastructure/staging/modules/ECS/main.tf
+++ infrastructure/staging/modules/ECS/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "apprunner" {
   name                 = "${local.resource_prefix}-apprunner"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
