f009dfc39d505e3631734fa30f0fdcce6aad0a72
TakaoSone <takao.sone.0616@gmail.com>
2022-10-22 00:56:27 +0900

    .

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,11 +6,11 @@ import { AppService } from './app.service';
 import { AppConfigModule } from './modules/app-config/app-config.module';
 import { GraphqlConfigService } from './modules/app-config/graphql-config.service';
 import { PrismaConfigService } from './modules/app-config/prisma-config.service';
+import { GoogleAuthenticationModule } from './modules/google-authentication/google-authentication.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { TokenAuthenticationModule } from './modules/token-authentication/token-authentication.module';
 import { UsersModule } from './modules/users/users.module';
-import { GoogleAuthenticationModule } from './modules/google-authentication/google-authentication.module';
 
 @Module({
   imports: [
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import MuiProvider from '../providers/Mui.provider';
 import ReactQueryProvider from '../providers/ReactQuery.provider';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Script src="https://accounts.google.com/gsi/client" strategy="afterInteractive" />
       <ReactQueryProvider>
         <MuiProvider>
           <Component {...pageProps} />
