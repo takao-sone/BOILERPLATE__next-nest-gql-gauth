60cb4374f660cc5737f694faee8ccf7e1215f76f
TakaoSone <takao.sone.0616@gmail.com>
2022-11-08 13:57:53 +0900

    .

--- frontend/src/fetchers.ts
+++ frontend/src/fetchers.ts
@@ -25,30 +25,24 @@ const BASE_GRAPHQL_CLIENT_OPTIONS: RequestInit = { credentials: 'include' };
 
 export const useLogIn = () => {
   const graphqlClient = new GraphQLClient(BASE_GRAPHQL_ENDPOINT, BASE_GRAPHQL_CLIENT_OPTIONS);
-
   return useLogInMutation(graphqlClient);
 };
 
 // TODO
 export const useGoogleRegisterUser = () => {
   const graphqlClient = new GraphQLClient(BASE_GRAPHQL_ENDPOINT, BASE_GRAPHQL_CLIENT_OPTIONS);
-
   const { mutateAsync } = useGoogleRegisterUserMutation(graphqlClient);
-
   return { mutateAsync };
 };
 
 // TODO
 export const useGoogleLogin = () => {
   const graphqlClient = new GraphQLClient(BASE_GRAPHQL_ENDPOINT, BASE_GRAPHQL_CLIENT_OPTIONS);
-
   const { mutateAsync } = useGoogleLoginMutation(graphqlClient);
-
   return { mutateAsync };
 };
 
 export const useAuthenticatedUser = () => {
-  // const authAccessToken = useRecoilValue(authAccessTokenState);
   const authAccessToken = useAuthAccessTokenValue();
   const headers: HeadersInit = {
     Authorization: `Bearer ${authAccessToken}`,
@@ -57,7 +51,6 @@ export const useAuthenticatedUser = () => {
     enabled: !!authAccessToken,
     retry: false,
   };
-
   const graphqlClient = new GraphQLClient(BASE_GRAPHQL_ENDPOINT, { headers });
   return useAuthenticatedUserQuery(graphqlClient, undefined, options, headers);
 };
@@ -66,7 +59,6 @@ export const getAuthenticatedUser = (authAccessToken: string) => {
   const headers: HeadersInit = {
     Authorization: `Bearer ${authAccessToken}`,
   };
-
   const graphqlClient = new GraphQLClient(BASE_GRAPHQL_ENDPOINT, { headers });
   return useAuthenticatedUserQuery.fetcher(graphqlClient)();
 };
