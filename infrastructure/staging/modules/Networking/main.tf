d913a3d2f0ac48895bce05ce3dc260e911781817
TakaoSone <takao.sone.0616@gmail.com>
2022-11-19 20:59:06 +0900

    .

--- infrastructure/staging/modules/Networking/main.tf
+++ infrastructure/staging/modules/Networking/main.tf
@@ -18,16 +18,16 @@ resource "aws_vpc" "vpc" {
 }
 
 # Subnet ===============================
-# resource "aws_subnet" "public_subnets_general" {
-#   count             = 2
-#   vpc_id            = aws_vpc.vpc.id
-#   availability_zone = data.aws_availability_zones.available.names[count.index]
-#   cidr_block        = var.public_subnet_general_cidrs[count.index]
+resource "aws_subnet" "public_subnets_general" {
+  count             = 2
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = var.public_subnet_general_cidrs[count.index]
 
-#   tags = {
-#     Name = "${local.resource_prefix}-public-subnet-general-${count.index + 1}"
-#   }
-# }
+  tags = {
+    Name = "${local.resource_prefix}-public-subnet-general-${count.index}"
+  }
+}
 
 resource "aws_subnet" "private_subnets_rds" {
   count             = 2
@@ -36,7 +36,7 @@ resource "aws_subnet" "private_subnets_rds" {
   cidr_block        = var.private_subnet_rds_cidrs[count.index]
 
   tags = {
-    Name = "${local.resource_prefix}-private-subnet-rds-${count.index + 1}"
+    Name = "${local.resource_prefix}-private-subnet-rds-${count.index}"
   }
 }
 
@@ -47,84 +47,86 @@ resource "aws_subnet" "private_subnets_elasticcache" {
   cidr_block        = var.private_subnet_elasticcache_cidrs[count.index]
 
   tags = {
-    Name = "${local.resource_prefix}-private-subnet-elasticcache-${count.index + 1}"
+    Name = "${local.resource_prefix}-private-subnet-elasticcache-${count.index}"
   }
 }
 
 # Route Table ===============================
-# resource "aws_route_table" "internet_rt" {
-#   vpc_id = aws_vpc.vpc.id
+resource "aws_route_table" "internet_rt" {
+  vpc_id = aws_vpc.vpc.id
 
-#   route {
-#     gateway_id = aws_internet_gateway.igw.id
-#     cidr_block = "0.0.0.0/0"
-#   }
+  route {
+    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+  }
 
-#   tags = {
-#     Name = "${local.resource_prefix}-internet-route"
-#   }
-# }
+  tags = {
+    Name = "${local.resource_prefix}-internet-route"
+  }
+}
 
-# resource "aws_route_table_association" "public_subnet_general_rt_association" {
-#   count          = length(aws_subnet.public_subnets_general)
-#   route_table_id = aws_route_table.internet_rt.id
-#   subnet_id      = aws_subnet.public_subnets_general[count.index].id
-# }
+resource "aws_route_table_association" "public_subnet_general_rt_association" {
+  count          = length(aws_subnet.public_subnets_general)
+  route_table_id = aws_route_table.internet_rt.id
+  subnet_id      = aws_subnet.public_subnets_general[count.index].id
+}
 
-# resource "aws_route_table" "public_nat_rts" {
-#   count  = length(aws_subnet.private_subnets_rds)
-#   vpc_id = aws_vpc.vpc.id
+# TODO: うまくいくやつ
+# TODO: @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+resource "aws_route_table" "public_nat_rts" {
+  count  = length(aws_subnet.private_subnets_rds)
+  vpc_id = aws_vpc.vpc.id
 
-#   route {
-#     nat_gateway_id = aws_nat_gateway.public_nats[0].id
-#     cidr_block     = "0.0.0.0/0"
-#   }
+  route {
+    nat_gateway_id = aws_nat_gateway.public_nats[0].id
+    cidr_block     = "0.0.0.0/0"
+  }
 
-#   tags = {
-#     Name = "${local.resource_prefix}-public-nat-route-${count.index}"
-#   }
-# }
-
-# resource "aws_route_table_association" "private_subnet_rds_rt_association" {
-#   count          = length(aws_subnet.private_subnets_rds)
-#   route_table_id = aws_route_table.public_nat_rts[count.index].id
-#   subnet_id      = aws_subnet.private_subnets_rds[count.index].id
-# }
+  tags = {
+    Name = "${local.resource_prefix}-public-nat-route-${count.index}"
+  }
+}
+resource "aws_route_table_association" "private_subnet_rds_rt_association" {
+  count          = length(aws_subnet.private_subnets_rds)
+  route_table_id = aws_route_table.public_nat_rts[count.index].id
+  subnet_id      = aws_subnet.private_subnets_rds[count.index].id
+}
+# TODO: @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 # Internet Gateway ===============================
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.vpc.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
 
-#   tags = {
-#     Name = "${local.resource_prefix}-igw"
-#   }
-# }
+  tags = {
+    Name = "${local.resource_prefix}-igw"
+  }
+}
 
 # NAT Gateway ===============================
-# resource "aws_nat_gateway" "public_nats" {
-#   count             = var.count_of_public_nats
-#   connectivity_type = "public"
-#   allocation_id     = aws_eip.eip_public_nats[count.index].id
-#   subnet_id         = aws_subnet.public_subnets_general[count.index].id
+resource "aws_nat_gateway" "public_nats" {
+  count             = var.count_of_public_nats
+  connectivity_type = "public"
+  allocation_id     = aws_eip.eip_public_nats[count.index].id
+  subnet_id         = aws_subnet.public_subnets_general[count.index].id
 
-#   tags = {
-#     Name = "${local.resource_prefix}-public-nat-${count.index}"
-#   }
+  tags = {
+    Name = "${local.resource_prefix}-public-nat-${count.index}"
+  }
 
-#   depends_on = [aws_internet_gateway.igw]
-# }
+  depends_on = [aws_internet_gateway.igw]
+}
 
 # Elastic IP ===============================
-# resource "aws_eip" "eip_public_nats" {
-#   count = var.count_of_public_nats
-#   vpc   = true
+resource "aws_eip" "eip_public_nats" {
+  count = var.count_of_public_nats
+  vpc   = true
 
-#   tags = {
-#     Name = "${local.resource_prefix}-eip-public-nat-${count.index}"
-#   }
+  tags = {
+    Name = "${local.resource_prefix}-eip-public-nat-${count.index}"
+  }
 
-#   depends_on = [aws_internet_gateway.igw]
-# }
+  depends_on = [aws_internet_gateway.igw]
+}
 
 # Security Group ==============================================
 resource "aws_security_group" "app_runner_vpc_connector_sg" {
--- infrastructure/staging/modules/Networking/outputs.tf
+++ infrastructure/staging/modules/Networking/outputs.tf
@@ -7,10 +7,6 @@ output "app_runner_vpc_connector_sg_id" {
   value = aws_security_group.app_runner_vpc_connector_sg.id
 }
 
-# output "app_runner_vpc_connector_target_subnet_ids" {
-#   value = aws_subnet.private_subnets_rds[*].id
-# }
-
 # RDS ===========================
 output "rds_vpc_connector_sg_id" {
   value = aws_security_group.rds_sg.id
