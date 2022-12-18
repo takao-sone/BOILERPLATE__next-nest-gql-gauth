1de4acd29b6d41d11688be16f3c0f7e00b7c10bd
TakaoSone <takao.sone.0616@gmail.com>
2022-12-18 23:30:07 +0900

    .

--- frontend/src/components/common/AppModal.tsx
+++ frontend/src/components/common/AppModal.tsx
@@ -8,6 +8,7 @@ const AppModal = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  console.log(handleOpen);
 
   return (
     <Backdrop
