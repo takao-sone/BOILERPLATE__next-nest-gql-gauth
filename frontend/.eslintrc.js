90f9716a93451bb592cd855962e38353d1d454ec
TakaoSone <takao.sone.0616@gmail.com>
2023-01-04 03:45:06 +0900

    .

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     'next/core-web-vitals',
     'prettier',
@@ -9,7 +11,8 @@ module.exports = {
   rules: {
     // TODO: '@tanstack/react-query'用
     'import/named': 'off',
-    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
       'warn',
