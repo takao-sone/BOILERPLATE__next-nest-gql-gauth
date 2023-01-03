82da901a2dea0bdd8e7631fb34a2e20fe02fe336
TakaoSone <takao.sone.0616@gmail.com>
2023-01-04 03:19:30 +0900

    .

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,7 +9,9 @@ module.exports = {
   rules: {
     // TODO: '@tanstack/react-query'用
     'import/named': 'off',
-    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    // 'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
       'warn',
